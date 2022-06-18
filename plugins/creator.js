820866114a857e3cfde3d512ce35aba4dccb4a22
RullMoonMC <107551141+RullMoonMC@users.noreply.github.com>
2022-06-19 06:07:24 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,10 +1,100 @@
-function handler(m) {
-  const data = global.owner.filter(([id, isCreator]) => id && isCreator)
-  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
+let { MessageType } = (await import('@adiwajshing/baileys')).default
+
+let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
+  let type = (args[0] || '').toLowerCase()
+  let _type = (args[0] || '').toLowerCase()
+
+//------- NOMOR
+  let nowner = `${nomorown.split`@`[0]}@s.whatsapp.net`
+  let teksnomor = `${htki} *OWNER* ${htka}
+• @${nomorown.split`@`[0]} •
+------- ${nameown} -------
+
+👾 *Catatan:*
+• Bleh Kenalan :v
+• Owner berhak blockir tanpa alasan
+• Berbicaralah yang sopan & tidak spam
+• Owner Hanya merespon yang berkaitan dengan BOT
+• No Telpon Kalo Gak Penting`
+
+👾 *Co-Owner*
+>> https://wa.me/6281539336141
+• Dilarang Spam
+• Owner Berhak Blokir Tanpa Alasan
+• Berbicaralah Dengan Sopan
+• Gaboleh Telpon
+
+//------------ BIO
+let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
+let teksbio = `${htki} *BIODATA* ${htka}
+*ɴᴀᴍᴇ:* Rull
+*ᴀɢᴇ:* ×
+*ᴄʟᴀss:* ×
+*sᴛᴀᴛᴜs:* Couple
+
+*ʙɪʀᴛʜᴅᴀʏ:* ×
+*ᴀᴅᴅʀᴇss:* ×
+
+🦋 *Whatsapp* wa.me/${nomorown}
+
+`
+  let teks = ' '
+const sections = [
+   {
+	title: `${htjava} OWNER –––––––––·•`,
+	rows: [
+	    {title: "✨ • Nomor", rowId: ".owner nomor"},
+	{title: "⭐ • Biodata", rowId: ".owner bio"},
+	]
+    },{
+	title: `${htjava} SUPPORT ME –––––––·•`,
+	rows: [
+	    {title: "🎀 • Donasi", rowId: ".owner nomor"},
+	]
+  },
+]
+
+const listMessage = {
+  text: teks,
+  footer: null,
+  title: `${htki} *⭐Owner* ${htka}`,
+  buttonText: "Klik Disinj !",
+  sections
 }
-handler.help = ['owner', 'creator']
-handler.tags = ['info']
 
-handler.command = /^(owner|creator)$/i
+  try {
+    if (/(creator|owner)/i.test(command)) {
+      const count = args[1] && args[1].length > 0 ? Math.min(99999999, Math.max(parseInt(args[1]), 1)) : !args[1] || args.length < 3 ? 1 : Math.min(1, count)
+        switch (type) {
+          case 'nomor':
+          conn.reply(m.chat, teksnomor, m, { contextInfo: { mentionedJid: [nowner] }})
+            break
+            case 'bio':
+          conn.sendHydrated(m.chat, teksbio, wm, ppown, "wa.me/" + nomorown, "💬 ᴄʜᴀᴛs", null,null, [["ᴅᴏɴᴀsɪ", '.donasi'], [null, null],[null,null]], m)
+            break
+            
+          default:
+            return await conn.sendMessage(m.chat, listMessage, { quoted: m, contextInfo: { mentionedJid: [m.sender] }})
+        }
+    } else if (/aoaooaoaooaoa/i.test(command)) {
+      const count = args[2] && args[2].length > 0 ? Math.min(99999999, Math.max(parseInt(args[2]), 1)) : !args[2] || args.length < 4 ? 1 :Math.min(1, count)
+      switch (_type) {
+        case 't':
+          break
+        case '':
+          break
 
-module.exports = handler
+        default:
+          return conn.sendButton( m.chat, caption, wm, null, [`⋮☰ Menu`, `.menu`], m)
+      }
+    }
+  } catch (err) {
+    m.reply("Error\n\n\n" + err.stack)
+  }
+}
+
+handler.help = ['owner', 'creaor']
+handler.tags = ['main', 'info']
+handler.command = /^(owner|creator)/i
+
+export default handler
